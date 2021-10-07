04bb4cb5ad163ad217727532ca5b14268513f9c8
sophiagavrila <msophiagavrila@gmail.com>
2021-10-07 00:18:02 -0400

    try api-url in front of api

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   title = "User Portal"
-  image = "/assets/network.png"
+  image = "api-ui/assets/network.png"
 
   constructor() { }
 
